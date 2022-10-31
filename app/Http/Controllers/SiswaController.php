d6bf7d4c7ba39461504f4ac9c4b07b1f5844f029
FirtaArieFirmansyah <firta10rpl1@gmail.com>
2022-10-31 09:17:18 +0700

    first commit

---
+++ app/Http/Controllers/SiswaController.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Siswa;
+
+class SiswaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $siswas = Siswa::all();
+        return view('mastersiswa', compact('siswas'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('tambahsiswa');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $message = [
+            'required' => ':attribute harus diisi yaa..',
+            'min' => ':attribute minimal :min yaa..',
+            'max' => ':attribute maksimal :max yaa..',
+            'numeric' => ':attribute harus diisi angka yaa..',
+            'mimes' => ':attribute harus bertipe jpg, jpeg, png yaa..',
+            'size' => ':file yang diupload harus maksimal size yaa..',
+            ];
+            $validatedData = $request->validate([
+                'nisn' => 'required|numeric',
+                'nama' => 'required|min:5',
+                'jk' => 'required',
+                'email' => 'required',
+                'alamat' => 'required',
+                'foto' => 'image|file',
+                'about' => 'required',
+            ], $message );
+            
+            Siswa::create($validatedData);
+            return redirect('/admin/mastersiswa');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $siswa = Siswa::find($id);
+        $projek = $siswa->project;
+        return view('showsiswa', ['siswa'=>$siswa,'projek' => $projek]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $siswa = Siswa::findOrFail($id);
+        return view('editsiswa', ['siswa'=>$siswa]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $validatedData = $request->validate([
+            'nisn' => 'required',
+            'nama' => 'required',
+            'jk' => 'required',
+            'email' => 'required',
+            'alamat' => 'required',
+            'foto' => 'image|file',
+            'about' => 'required'
+        ]);
+
+        $siswas=Siswa::where('id', $id)
+            ->update($validatedData);
+
+        return redirect()->route('mastersiswa.index')->with('success', 'Data Updated Successfully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Siswa::destroy($id);
+        return redirect('admin/mastersiswa');
+    }
+}
